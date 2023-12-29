824b62a99d8b74671216731c9ccfa49c36187a42
Anish <manandharanish419@gmail.com>
2023-12-29 14:46:37 +0545

    INITIAL COMMIT

---
+++ Assets/ZombieFallDetector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ZombieFallDetector : MonoBehaviour {
+
+	public GameMananger gameManager;
+
+	void OnEnable () {
+
+	}
+
+	void OnDisable () {
+			
+	}
+
+	void OnTriggerEnter(Collider other){
+		GameObject fallenZombie = other.gameObject;
+		gameManager.HandleFallenZombie (fallenZombie);
+	}
+
+}
